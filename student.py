475b6162b9f6a611e888d74f8d92e5a04f392159
Joanna Gorska <gorska919@gmail.com>
2021-11-30 10:02:48 +0000

    Add test_students.py file and wrote first test to se if full_name method returns the string containing frist and second name

--- student.py
+++ student.py
@@ -1,9 +1,10 @@
 from datetime import date, timedelta
 
+
 class Student:
     """ A Student class as a basee for method testing """
     def __init__(self, first_name, last_name):
-        self._first_name  = first_name
+        self._first_name = first_name
         self._last_name = last_name
         self._start_date = date.today()
         self.end_date = date.today() + timedelta(days=365)
